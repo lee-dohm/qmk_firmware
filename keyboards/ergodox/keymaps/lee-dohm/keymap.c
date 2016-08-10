5533d30ba4fe58f9e6a6578894266474f411ac53
Lee Dohm <lee@lee-dohm.com>
2016-08-09 22:30:40 -0700

    Fix the brightness control keys

--- keyboards/ergodox/keymaps/lee-dohm/keymap.c
+++ keyboards/ergodox/keymaps/lee-dohm/keymap.c
@@ -90,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_F22, KC_TRNS,
-       KC_F16,
+       KC_F15, KC_TRNS,
+       KC_F14,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Numeric Keypad
